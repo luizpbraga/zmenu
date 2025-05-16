9e6e5b5a4c169b08077c3d2d719d0e38dbec06be
Luiz Alberto Portilho Braga <portilho.braga@gmail.com>
2025-05-16 19:41:43 +0000

    new window example

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,8 @@
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
 const c = @cImport(@cInclude("gtk-4.0/gtk/gtk.h"));
+const std = @import("std");
+
 /// Could not get `g_signal_connect` to work. Zig says "use of undeclared identifier". Reimplemented here
 pub fn gSignalConnect(instance: c.gpointer, detailed_signal: [*c]const c.gchar, c_handler: c.GCallback, data: c.gpointer) c.gulong {
     var zero: u32 = 0;
@@ -28,6 +30,33 @@ fn onAnctivate(app: *c.GtkApplication, user_data: c.gpointer) callconv(.C) void 
 pub fn main() !void {
     const app = c.gtk_application_new("com.example.GTK", c.G_APPLICATION_FLAGS_NONE);
     defer c.g_object_unref(app);
-    _ = gSignalConnect(app, "activate", @ptrCast(&onAnctivate), null);
-    _ = c.g_application_run(@ptrCast(app), 0, 0);
+    _ = gSignalConnect(app, "activate", @ptrCast(&activateWindow), null);
+    const stats = c.g_application_run(@ptrCast(app), 0, 0);
+    std.debug.print("STATUS: {}", .{stats});
+
+    // window whith a button
+    // const app = c.gtk_application_new("com.example.GTK", c.G_APPLICATION_FLAGS_NONE);
+    // defer c.g_object_unref(app);
+    // _ = gSignalConnect(app, "activate", @ptrCast(&onAnctivate), null);
+    // _ = c.g_application_run(@ptrCast(app), 0, 0);
 }
+
+fn activateWindow(app: *c.GtkApplication, user_data: c.gpointer) callconv(.C) void {
+    _ = user_data;
+    // c.g_print("GtkApplication is activated.\n");
+    //
+    // * the window will only bee desplayed with gtk_window_set_application
+    // const window = c.gtk_window_new();
+    // c.gtk_window_set_application(@ptrCast(window), @ptrCast(app));
+
+    // the fallowing will display the window automatically
+    const window = c.gtk_application_window_new(app);
+
+    // style
+    c.gtk_window_set_title(@ptrCast(window), "Test");
+    c.gtk_window_set_default_size(@ptrCast(window), 400, 300);
+
+    c.gtk_window_present(@ptrCast(window));
+}
+
+test "signals" {}
